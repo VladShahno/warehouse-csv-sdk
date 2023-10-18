a6e5e2d67aef77e5eb3fb760b42f169003aab378
Vladyslav Shakhno <vladyslav.shakhno@cengage.com>
2023-10-18 14:41:38 +0300

    05 - updated

--- src/main/java/warehouse/com/csv/Application.java
+++ src/main/java/warehouse/com/csv/Application.java
@@ -2,8 +2,10 @@ package warehouse.com.csv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("warehouse.com")
 public class Application {
 
   public static void main(String[] args) {
